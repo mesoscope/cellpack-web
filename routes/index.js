12da2f0804f25959d26131f06098bcee14a17098
Russell Mays <hi@russellmays.com>
2014-04-07 15:45:08 -0700

    init commit

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.autopack = function(req, res) {
+  res.render('autopack', {title: 'The new autopack!'});
+};
+
+exports.userlist = function(db) {
+  return function(req, res) {
+    var collection = db.get('usercollection');
+    collection.find({}, {}, function(e, docs) {
+      res.render('userlist', {"userlist": docs});
+    });
+  };
+};
+
+exports.newuser = function(req, res) {
+  res.render('newuser', {title: 'Add New User'});
+};
+
+exports.adduser = function(db) {
+  return function(req, res) {
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+    var collection = db.get('usercollection');
+
+    collection.insert({"username" : userName, "email" : userEmail}, 
+			function (err, doc) {
+				if (err) {
+					res.send("There was a problem adding the information to the database.");} else {
+				res.location("userlist");
+				res.redirect("userlist");}
+			});
+		}
+	}
