ee8da01176e2aee3e719992518030fa61d6e2cc6
Russell Mays <hi@russellmays.com>
2014-04-15 14:18:00 -0700

    removing newuser residuals

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index(db));
 app.get('/recipes/:recipename', routes.recipe(db));
-app.post('/adduser', routes.adduser(db));
+app.get('/newrecipe', function(req, res) {res.render('newrecipe', {})});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -16,19 +16,3 @@ exports.recipe = function(db) {
     });
   };
 };
-
-exports.adduser = function(db) {
-  return function(req, res) {
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
-    var collection = db.get('usercollection');
-
-    collection.insert({"username" : userName, "email" : userEmail}, 
-			function (err, doc) {
-				if (err) {
-					res.send("There was a problem adding the information to the database.");} else {
-				res.location("userlist");
-				res.redirect("userlist");}
-			});
-		}
-	}
