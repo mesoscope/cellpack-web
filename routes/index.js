bd8251cffaf2de23c78e7f9bedb12cf7836482e7
Russell Mays <hi@russellmays.com>
2014-06-19 18:01:25 -0700

    shit just works

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ exports.index = function(db) {
   return function(req, res) {
     var collection = db.get("recipes");
     collection.find({}, function(e, docs) {
-      var recipeNames = helpers.getDocNames(docs);
-      res.render('index', {'title': 'cellPACK', 'recipeNames': recipeNames});
+      var recipeData = helpers.getRecData(docs);
+      res.render('index', {'title': 'cellPACK', 'recipeData': recipeData});
     });
   };
 };
@@ -14,13 +14,7 @@ exports.modify = function(db) {
   return function(req, res) {
     var collection = db.get('recipes');
     collection.find({}, function(e, docs) {
-      var recipeNames = helpers.getDocNames(docs);
-      var recipeData = {};
-      var rnLength = recipeNames.length;
-      for (var i = 0; i < rnLength; i++) {
-        var individualVers = helpers.getStringVersions(docs, recipeNames[i]);
-        recipeData[recipeNames[i]] = individualVers;
-      }
+      var recipeData = helpers.getRecData(docs);
       if (typeof req.body['recname'] != 'undefined') {
         var versArray = req.body['recversion'].split('.');
         var individualIDTree = helpers.getIdentifierTree(docs, helpers.constructIdentifier(req.body['recname'], versArray[0], versArray[1], versArray[2]));
@@ -39,7 +33,7 @@ exports.newrecipe = function(db) {
     var collection = db.get('recipes');
     collection.find({}, function(e, docs) {
       var recipeNames = helpers.getDocNames(docs);
-      res.render('newrecipe', {"recipeNames": recipeNames});
+      res.render('newrecipe', {'title': 'New cellPACK Receipe', 'recipeNames': recipeNames});
     });
   };
 };
