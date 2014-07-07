26bc843175b62c0bd4bece72874b8582af282485
Russell Mays <hi@russellmays.com>
2014-07-07 15:05:15 -0700

    ajax sorta working

--- public/javascripts/newrow.js
+++ public/javascripts/newrow.js
@@ -1,15 +1,28 @@
 $(document).ready(function() {
-  $("#commitButton").hide();
-  $(".tableButton").click(function() {
-    // show the commit button
-    $.post("/modified", 
-    {
-      name:"testName",
-      city:"testCity"
-    }, 
-    function(data) {
-      alert(data);
-      $("#commitButton").show();
+  $('#commitButton').hide();
+  $('.saveButton').click(function() {
+
+    var editedDict = {};
+    editedDict['name'] = $(this).data('recname');
+    editedDict['optionValues'] = [];
+
+    $('.'+editedDict['name']+'-valuerow').last().children().each(function(i) {
+      
+      if (i != 0) {
+        editedDict['optionValues'].push($(this).children().val());
+      } else {
+        editedDict['version'] = $(this).text();
+      }
+    });
+
+
+    $.post('/modified', editedDict, function(data) {
+      if (typeof data == 'string') {
+        alert(data);
+      } else {
+        $('.'+data['name']+'-valuerow').last().remove();
+        $('#commitButton').show();
+      }
     });
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ exports.modify = function(db) {
       if (typeof req.body['recname'] != 'undefined') {
         var versArray = req.body['recversion'].split('.');
         var individualIDTree = helpers.getIdentifierTree(docs, helpers.constructIdentifier(req.body['recname'], versArray[0], versArray[1], versArray[2]));
-        res.render('modifyt', {'title': 'Modify Recipe', 'topLevel': req.body['recname'], 'recipeData': recipeData, 'tableTree': individualIDTree});
+        res.render('modifyt', {'title': 'Modify Recipe', 'topName': req.body['recname'], 'topVersion':req.body['recversion'], 'recipeData': recipeData, 'tableTree': individualIDTree});
       } else {
         res.render('modify', {'title': 'Modify Recipe', 'recipeData': recipeData});
       }
@@ -28,14 +28,38 @@ exports.modify = function(db) {
 
 exports.modified = function(db) {
   return function(req, res) {
-    console.log(req.body);
-    res.send({blargh: 'blargh'});
+
+    var collection = db.get('recipes');
+    var submittedID = req.body['name']+'-'+req.body['version'].split('.').join('_');
+    collection.find({'identifier': submittedID}, function(e, docs) {
+      var different = false;
+      for (var i = 0; i < req.body['optionValues'].length; i++) {
+        if (!different && req.body['optionValues'][i] != docs[0]['options'][Object.keys(docs[0]['options'])[i]]) {
+          different = true;
+        }
+      }
+      
+
+      if (different) {
+	var newR = {};
+        var newVersion = req.body['version'].split('.')
+        newVersion[2] = parseInt(newVersion[2]) + 1
+        newR['name'] = req.body['name'];
+        newR['version'] = newVersion.join('.');
+        newR['optionValues'] = req.body['optionValues'];
+        //console.log(newR);
+        res.send(newR);
+      } else {
+        res.send('Please change recipe before saving!');
+      }
+
+    });
   };
 };
 
 exports.committed = function(db) {
   return function(req, res) {
     console.log(req.body);
-    res.redirect('/');
+    res.redirect('/modify');
   };
 };
