becaad0af86e69fa9fcd6403bd3f87e6aa5719c1
Russell Mays <hi@russellmays.com>
2014-07-08 17:22:04 -0700

    dynamic rows and shit

--- public/javascripts/tabler.js
+++ public/javascripts/tabler.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
+
+  $('#commit').hide();
   var recn = $('select.recn');
   var recv = $('select.recv');
   $('#loader').click(function() {
     $('#tableForm').empty();
-    if (recv.val()) {
-      // data is an array
-      $.post('/tabler', {'recipename': recn.val(), 'recipevers': recv.val()}, function(data) {
-        $.each(data, function(i, val) {
-          $('#tableForm').append('<div><div style="float:left", align="center"><h2>'+val['tablename']+'</h2></div><div style="float:right" align="center"><button type="button" class="saveButton" data-recname='+val['tablename']+'>Save as New Version</button></div><div style="float:none" align="center"></div></div>');
+    $.post('/tabler', {'recipename': recn.val(), 'recipevers': recv.val()}, function(data) {
+      $.each(data, function(i, val) {
+        var recTable = '<table border="1" id="'+val['tablename']+'-table">';
+        var recLabels = '<tr id="'+val['tablename']+'-labelrow"><th>Version</th>';
+        var recValues = '<tr id="'+val['tablename']+'-valuerow"><td>'+val['tableversion']+'</td>';
+        $.each(Object.keys(val['tableoptions']), function(i, val2) {
+          recLabels = recLabels + '<th>'+ val2 + '</th>';
+          recValues = recValues + '<td><input type="text" placeholder="' + val['tableoptions'][val2] + '" val=""></td>';
         });
+        recTable = recTable + recLabels + '</tr>' + recValues + '</tr>' +'</table>';
+        $('#tableForm').append('<div><div style="float:left", align="center"><h2>'+val['tablename']+'</h2></div><div style="float:right" align="center"><button type="button" class="saver" data-recname="'+val['tablename']+'">Save as New Version</button></div><div style="float:none" align="center">'+recTable+'</div></div><br><br>');
       });
-    } else {
-      alert('Please select a recipe and version!');
-    }
+    });
+    $.getScript('javascripts/newrow.js');
   });
 });
