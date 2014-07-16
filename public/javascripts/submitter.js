178f5740e8793d7a51e0e7adb56de60f04ba95a4
Russell Mays <hi@russellmays.com>
2014-07-15 17:36:17 -0700

    almost to tree commit

--- public/javascripts/submitter.js
+++ public/javascripts/submitter.js
@@ -1,48 +1,48 @@
 $('#submitter').click(function() {
-  var recName = $('#recTableHeader').children().html();
-  var recVersion = 'UNDEFINED';
-  var recOptions = {};
+    var recName = $('#recTableHeader').children().html();
+    var recVersion = 'UNDEFINED';
+    var recOptions = {};
 
-  // option labels
-  var optionLabels = [];
-  $('#'+recName+'-referencelabels').children().each(function(index) {
-    if (index != 0) {
-      optionLabels.push($(this).html());
+    // option labels
+    var optionLabels = [];
+    $('#'+recName+'-referencelabels').children().each(function(index) {
+        if (index != 0) {
+            optionLabels.push($(this).html());
+        }
+    });
+    
+    // option values
+    var edited = false;
+    var optionValues = [];
+    $('#'+recName+'-dynamicvalues').children().each(function(index) {
+        if (index == 0) {
+            recVersion = $(this).html();
+        } else {
+            if ($(this).children().val() != '') {
+                edited = true;
+                optionValues.push($(this).children().val());
+            } else {
+                optionValues.push($(this).children().attr('placeholder'));
+            }
+        }
+    });
+
+    for (var i = 0; i < optionValues.length; i++) {
+        recOptions[optionLabels[i]] = optionValues[i];
     }
-  });
 
-  // option values
-  var edited = false;
-  var optionValues = [];
-  $('#'+recName+'-dynamicvalues').children().each(function(index) {
-    if (index == 0) {
-      recVersion = $(this).html();
+    var topLevel = '';
+    $('.sideLink').each(function(index) {
+        if (index == 0) {
+            topLevel = $(this).html()+'-'+$(this).data('vers').split('.').join('_');
+        }
+    });
+     
+    if (edited) {
+        var newIdentifier = recName + '-' + recVersion.split('.').join('_');
+        var newRecipe = {'identifier': newIdentifier, 'options': recOptions};
+        $.post('/commit', {'newRecipe': newRecipe, 'topLevel': topLevel});
     } else {
-      if ($(this).children().val() != '') {
-        edited = true;
-        optionValues.push($(this).children().val());
-      } else {
-        optionValues.push($(this).children().attr('placeholder'));
-      }
+        alert('Please edit recipe before updating!');
     }
-  });
-
-  for (var i = 0; i < optionValues.length; i++) {
-    recOptions[optionLabels[i]] = optionValues[i];
-  }
-
-  var hierarchy = [];
-  $('.sideLink').each(function(index) {
-    if (hierarchy.indexOf(recName) < 0) {
-      hierarchy.push($(this).html());
-    }
-  });
-
-  if (edited) {
-    var newIdentifier = recName + '-' + recVersion.split('.').join('_');
-    var newRecipe = {'identifier': newIdentifier, 'options': recOptions};
-    $.post('/commit', {'newRecipe': newRecipe, 'hierarchy': hierarchy});
-  } else {
-    alert('Please edit recipe before updating!');
-  }
 });
--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,70 @@
 var helpers = require('utils');
 
 exports.index = function(db) {
-  return function(req, res) {
-    var collection = db.get('recipes');
-    collection.find({}, function(e, docs) {
-      var recNames = helpers.getDocNames(docs);
-      res.render('index', {'title': 'cellPACK', 'recNames': recNames});
-    });
-  };
+    return function(req, res) {
+        var collection = db.get('recipes');
+        collection.find({}, function(e, docs) {
+            var recNames = helpers.getDocNames(docs);
+            res.render('index', {'title': 'cellPACK', 'recNames': recNames});
+        });
+    };
 };
 
 exports.versioner = function(db) {
-  return function(req, res) {
-    var collection = db.get('recipes');
-    collection.find({}, function(e, docs) {
-      var possibleVersions = helpers.getStringVersions(docs, req.body['recipename']);
-      res.send(possibleVersions);
-    });
-  };
+    return function(req, res) {
+        var collection = db.get('recipes');
+        collection.find({}, function(e, docs) {
+            var possibleVersions = helpers.getStringVersions(docs, req.body['recipename']);
+            res.send(possibleVersions);
+        });
+    };
 };
 
 exports.modify = function(db) {
-  return function(req, res) {
-    var collection = db.get('recipes');
-    collection.find({}, function(e, docs) {
-      var recNames = helpers.getDocNames(docs);
-      res.render('modify', {'title': 'Modify Recipe', 'recNames': recNames});
-    });
-  };
+    return function(req, res) {
+        var collection = db.get('recipes');
+        collection.find({}, function(e, docs) {
+            var recNames = helpers.getDocNames(docs);
+            res.render('modify', {'title': 'Modify Recipe', 'recNames': recNames});
+        });
+    };
 };
+
 // clean this up with mongoose?
 exports.hierarchy = function(db) {
-  return function(req, res) {
-    var collection = db.get('recipes');
-    collection.find({}, function(e, docs) {
-      var reqID = req.body['recipename']+'-'+req.body['recipevers'].split('.').join('_');
-      var identifierTree = helpers.getIdentifierTree(docs, reqID);
-      res.send(identifierTree);
-    });
-  };
+    return function(req, res) {
+        var collection = db.get('recipes');
+        collection.find({}, function(e, docs) {
+            var reqID = req.body['recipename']+'-'+req.body['recipevers'].split('.').join('_');
+            var identifierTree = helpers.getIdentifierTree(docs, reqID);
+            res.send(identifierTree);
+        });
+    };
 };
 
 exports.tabler = function(db) {
-  return function(req, res) {
-    var newID = req.body['recipename']+'-'+req.body['recipevers'].split('.').join('_');
-    var collection = db.get('recipes');
-    collection.find({'identifier': newID}, function(e, docs) {
-      res.send(docs[0]);
-    });
-  };
+    return function(req, res) {
+        var newID = req.body['recipename']+'-'+req.body['recipevers'].split('.').join('_');
+        var collection = db.get('recipes');
+        collection.find({'identifier': newID}, function(e, docs) {
+            res.send(docs[0]);
+        });
+    };
 };
 
 exports.commit = function(db) {
-  return function(req, res) {
-    console.log(req.body);
-    var previousVers = req.body['identifier'].split('-')[1].split('_');
-    previousVers[2] = parseInt(previousVers[2]) - 1;
-    var previousID = req.body['identifier'].split('-')[0]+'-'+previousVers.join('_');
-    var collection = db.get('recipes');
-    collection.find({'identifier': previousID}, function(e, docs) {
-      req.body['children'] = docs[0]['children'];
-      collection.insert(req.body, function(e, docs) {
-        // this doesn't work
-        res.redirect('/modify');
-      });
-    });
-  };
+    return function(req, res) {
+        console.log(req.body);
+        
+        var previousVers = req.body['newRecipe']['identifier'].split('-')[1].split('_');
+        previousVers[2] = parseInt(previousVers[2]) - 1;
+        var previousID = req.body['newRecipe']['identifier'].split('-')[0]+'-'+previousVers.join('_'); 
+
+        var collection = db.get('recipes');
+        collection.find({}, function(e, docs) {
+            // FIX THIS 
+            //req.body['newRecipe']['children'] = docs[0]['children'];
+            console.log(helpers.getDescendents(docs, req.body['topLevel'], previousID));
+        });
+    };
 };
