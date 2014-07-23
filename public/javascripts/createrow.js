59f9f109defa0ef8a3f0e9e804736010ecdd9558
Russell Mays <hi@russellmays.com>
2014-07-23 14:21:26 -0700

    recipe options works

---
+++ public/javascripts/createrow.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#recOptioner').click(function() {
+        $('#recOptionFields').append('<br><input type="text" class="optionLabel"><input type="text" class="optionVal">');
+    });
+});
