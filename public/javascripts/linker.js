80ddba567204cb67d8f1fb96b382fd326e14b0de
Russell Mays <hi@russellmays.com>
2014-07-31 12:35:54 -0700

    updated submitter

--- public/javascripts/linker.js
+++ public/javascripts/linker.js
@@ -1,5 +1,4 @@
 $('.sideLink').click(function() {
-    alert('Clicked!');
     window.recipeName = $(this).html();
     window.recipeVersion = $(this).data('vers');
     $.getScript('javascripts/tabler.js');
--- public/javascripts/tabler.js
+++ public/javascripts/tabler.js
@@ -26,6 +26,51 @@ $.post('/tabler', {'recipename': recipeName, 'recipevers': recipeVersion}, funct
     
     $('#recipeForm').append(recTable+'<br><br>');
     $('#recipeForm').append('<button id="submitter" type="button"> Update </button><br><br>');
-});
+    
+    $('#submitter').click(function() {
+        var recName = $('#recTableHeader').children().html();
+        var recVersion = 'UNDEFINED';
+        var recOptions = {};
 
-$.getScript('javascripts/submitter.js');
+        var optionLabels = [];
+        $('#'+recName+'-referencelabels').children().each(function(index) {
+            if (index != 0) {
+                optionLabels.push($(this).html());
+            }
+        });
+    
+        var edited = false;
+        var optionValues = [];
+        $('#'+recName+'-dynamicvalues').children().each(function(index) {
+            if (index == 0) {
+                recVersion = $(this).html();
+            } else {
+                if ($(this).children().val() != '') {
+                    edited = true;
+                    optionValues.push($(this).children().val());
+                } else {
+                    optionValues.push($(this).children().attr('placeholder'));
+                }
+            }
+        });
+
+        for (var i = 0; i < optionValues.length; i++) {
+            recOptions[optionLabels[i]] = optionValues[i];
+        }
+
+        var topLevel = '';
+        $('.sideLink').each(function(index) {
+            if (index == 0) {
+                topLevel = $(this).html()+'-'+$(this).data('vers').split('.').join('_');
+            }
+        });
+     
+        if (edited) {
+            var newIdentifier = recName + '-' + recVersion.split('.').join('_');
+            var newRecipe = {'recipeIdentifier': newIdentifier, 'recipeOptions': recOptions};
+            $.post('/commit', {'newRecipe': newRecipe, 'topLevel': topLevel});
+        } else {
+            alert('Please edit recipe before updating!');
+        }
+    });
+});
