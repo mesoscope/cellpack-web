32c7ad587e9aaf56af80d855a0b6f496741f3e73
Russell Mays <hi@russellmays.com>
2014-08-05 11:31:43 -0700

    coloring and modifying

--- public/javascripts/dev.js
+++ public/javascripts/dev.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $('#createChild').hide();
     $('#loader').click(function() {
         window.recipeName = $('select.recn').val();
         window.recipeVersion = $('select.recv').val();
@@ -7,8 +8,18 @@ $(document).ready(function() {
             $.each(data, function(i, val) {
                 $('#recipeList').append('<button class="sideLink" data-vers="'+val['tableversion']+'">'+val['tablename']+'</button><br>');
             });
+            $('.sideLink').first().css('background-color', 'blue');
+            $('#createChild').show();
         });
         $.getScript('javascripts/tabler.js');
         $.getScript('javascripts/linker.js');
+    });
+
+    $('#createChild').click(function() {
+        alert('Adding Child!');
+    });
+
+    $('#childer').click(function() {
+        alert('Childer Pressed');
     });
 });
--- public/javascripts/linker.js
+++ public/javascripts/linker.js
@@ -1,5 +1,7 @@
 $('.sideLink').click(function() {
     window.recipeName = $(this).html();
     window.recipeVersion = $(this).data('vers');
+    $('.sideLink').not($(this)).css('background-color', 'white');
+    $(this).css('background-color', 'blue');
     $.getScript('javascripts/tabler.js');
 });
