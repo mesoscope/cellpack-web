64287878e0e881487ae5c62504a9854ac56e1570
Russell Mays <russell.c.mays@gmail.com>
2015-05-27 05:45:39 +0000

    building views

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,12 +1,25 @@
-var TreeView = Backbone.View.extend({
-    el: 'recipetree',
-    initialize: function() {
-	this.render();
-    },
-    render: function() {
-	this.$el.html("Hello World!");
-    }
-});
+$(document).ready(function() {
+    //Recipe = Backbone.Model.extend({});
 
-var treeView = new TreeView();
+    var RecipeView = Backbone.View.extend({
+	el: "#recipeContainer",
+	initialize: function() {
+	    this.render();
+	},
+	render: function() {
+	    this.$el.html("Recipe View Loaded");
+	}
+    });
+    var recipeView = new RecipeView();
 
+    var TreeView = Backbone.View.extend({
+	el: "#treeContainer",
+	initialize: function() {
+	    this.render();
+	},
+	render: function() {
+	    this.$el.html("Tree View Loaded");
+	}
+    });
+    var treeView = new TreeView();
+})
